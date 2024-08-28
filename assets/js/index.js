ebef98613018eceb2a36f9d64d3fe41cf646e248
Dinesh18042001 <dinesh@1804200182@gmail.com>
2024-08-28 18:31:33 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+let nav = document.querySelector(".navbar");
+window.onscroll = function (){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+};
+
+$('.brand_slide').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+
+
+
+
+document.addEventListener('scroll', function() {
+    const article = document.querySelector('.blog-article');
+    const progressBar = document.querySelector('.nav__scroll-bar');
+  
+    const articleRect = article.getBoundingClientRect();
+    const articleTop = articleRect.top;
+    const articleHeight = articleRect.height;
+  
+    const windowHeight = window.innerHeight;
+    const totalScroll = articleHeight - windowHeight;
+  
+    if (articleTop < 0 && Math.abs(articleTop) <= totalScroll) {
+      const progress = (Math.abs(articleTop) / totalScroll) * 100;
+      progressBar.style.width = `${progress}%`;
+    } else if (Math.abs(articleTop) > totalScroll) {
+      progressBar.style.width = '100%';
+    } else {
+      progressBar.style.width = '0%';
+    }
+  });
+  
